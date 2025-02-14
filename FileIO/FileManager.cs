0e1835452a947d4b9ea7c7564a8c387be233afe0
Kyle S <kstephenson1@my.wctc.edu>
2025-02-14 02:36:58 -0600

    Condensed Json Serializer Options.

--- FileIO/FileManager.cs
+++ FileIO/FileManager.cs
@@ -9,21 +9,32 @@ using w4_assignment_ksteph.DataTypes;
 public class FileManager
 {
 
-    private FileType _fileType = Config.DEFAULT_FILE_TYPE;
-    public ICharacterIO GetFileType()
+    private static FileType _fileType = Config.DEFAULT_FILE_TYPE;
+
+    private ICharacterIO GetFileType() // Checks to see what the current file type is set to and execute the proper file system.
     {
-        switch (_fileType)
+        return _fileType switch
         {
-            case FileType.Csv:
-                return new CsvFileHandler();
-                break;
-            case FileType.Json:
-                return new JsonFileHandler();
-                break;
-            default:
-                throw new NullReferenceException("Error: File type not found in FileManager.GetFileType()");
+            FileType.Csv => new CsvFileHandler(),
+            FileType.Json => new JsonFileHandler(),
+            _ => throw new NullReferenceException("Error: File type not found in FileManager.GetFileType()"),
+        };
+    }
+
+    public static void SwitchFileType()
+    {
+        Console.Clear();
+        if (_fileType == FileType.Csv)
+        {
+            Console.WriteLine("File format set to Json.");
+            _fileType = FileType.Json;
+        } else
+        {
+            Console.WriteLine("File format set to Csv.");
+            _fileType = FileType.Csv;
         }
     }
+
     // FileManager contains redirects to functions that assist with Csv functions. 
     public List<Character> ImportCharacters() => GetFileType().ReadCharacters();
     public void ExportCharacters(List<Character> characters) => GetFileType().WriteCharacters(characters);
--- FileIO/Json/JsonFileHandler.cs
+++ FileIO/Json/JsonFileHandler.cs
@@ -9,30 +9,30 @@ public class JsonFileHandler : ICharacterIO
     // JsonFileHandler is used to convert bwtween characters and json format
 
     private const string JSON_FILE_PATH = "Files/Input.json";
+    private JsonSerializerOptions _options = new();
+
+    public JsonFileHandler()
+    {
+        _options.Converters.Add(new JsonInventoryConverter());   // Using a custom converter to convert json string -> Inventory
+        _options.Converters.Add(new JsonIntConverter());         // Using a custom converter to convert json string -> int
+        _options.WriteIndented = true;                           // Writes the json file in indented format.
+    }
 
     public List<Character> ReadCharacters()
     {
-        JsonSerializerOptions options = new();
-        options.Converters.Add(new JsonInventoryConverter());   // Using a custom converter to convert json string -> Inventory
-        options.Converters.Add(new JsonIntConverter());         // Using a custom converter to convert json string -> int
 
         using (StreamReader reader = new(JSON_FILE_PATH))       // reads from the json file and returns a list of characters.
         {
             string json = reader.ReadToEnd();
-            return JsonSerializer.Deserialize<List<Character>>(json, options);
+            return JsonSerializer.Deserialize<List<Character>>(json, _options)!;
         }
     }
 
     public void WriteCharacters(List<Character> characters)
     {
-        JsonSerializerOptions options = new();
-        options.Converters.Add(new JsonInventoryConverter());   // Using a custom converter to convert json string -> Inventory
-        options.Converters.Add(new JsonIntConverter());         // Using a custom converter to convert json string -> int
-        options.WriteIndented = true;                           // Writes the json file in indented format.
-
         using (StreamWriter writer = new(JSON_FILE_PATH))       // Takes a list of characters and writes to the json file
         {
-            writer.WriteLine(JsonSerializer.Serialize<List<Character>>(characters, options));
+            writer.WriteLine(JsonSerializer.Serialize<List<Character>>(characters, _options));
         }
     }
 }
